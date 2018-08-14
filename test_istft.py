3e196eb72e5c77e428d5e96490fecbffdda01f77
Dario Cazzani <dcazzani@cisco.com>
2018-08-14 10:06:26 -0700

    Test with real audio

--- test_istft.py
+++ test_istft.py
@@ -2,8 +2,12 @@ import functools
 import tensorflow as tf
 import tensorflow.contrib
 import matplotlib.pyplot as plt
+import sys
 import numpy as np
+from signal_processing import wav_to_floats, floats_to_wav
+
 np.random.seed(42)
+
 frame_lengths = [45, 50, 70, 108, 136, 160]
 frame_steps = [25, 30, 50, 64, 86, 123]
 resamplings= [32, 16, 8, 4, 2, 1]
@@ -16,17 +20,21 @@ fft_lengths = [31, 62, 127, 255]
 
 params = list(zip(resamplings, fft_lengths, frame_lengths, frame_steps))
 
-import sys
+audio, fs = wav_to_floats('test.wav')
+audio = audio-np.mean(audio)
+audio /= np.max(np.abs(audio))
+audio = np.reshape(audio, (1, -1))
+AUDIO_LENGTH = 16000
+
 if __name__ == '__main__':
 	show = False
 	if len(sys.argv) > 1:
 		show = True
 	for resampling, fft_length, frame_length, frame_step in params:
-		AUDIO_LENGTH = 16000
 
-		x = tf.placeholder(tf.float32, shape=[AUDIO_LENGTH])
+		x = tf.placeholder(tf.float32, shape=[1, AUDIO_LENGTH])
 
-		resampled_x = x[::resampling]
+		resampled_x = x[:, ::resampling]
 		stft = tf.contrib.signal.stft(
 			resampled_x,
 			frame_length=frame_length,
@@ -50,9 +58,10 @@ if __name__ == '__main__':
 		)
 
 		with tf.Session() as sess:
-			audio = np.random.randn(AUDIO_LENGTH)
 			reconstructed, resampled_x_val = sess.run([istft, resampled_x], feed_dict={x: audio})
 
+			reconstructed = np.squeeze(reconstructed)
+			resampled_x_val = np.squeeze(resampled_x_val)
 			plt.plot(resampled_x_val[:len(reconstructed)], label='orig')
 			plt.plot(reconstructed, label='reconstructed')
 			plt.legend()
@@ -64,3 +73,5 @@ if __name__ == '__main__':
 			print("FFT LENGTH: {} - STFT shape: {} - Error: {:.6f}".format(fft_length,
 												stft_shape,
 												np.mean(np.abs(short_resampled_x[padding: -padding] - reconstructed[padding: -padding]))))
+
+		floats_to_wav('test_{}.wav'.format(resampling), resampled_x_val, len(resampled_x_val))
